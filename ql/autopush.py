c23894850d5c9c37c313ee6f84f9e504711f785e
kimcrowing <50307403+kimcrowing@users.noreply.github.com>
2024-04-20 11:45:53 +0800

    Update autopush.py

--- ql/autopush.py
+++ ql/autopush.py
@@ -12,7 +12,7 @@ import datetime
 # 获取当前的日期，并转换为字符串，格式为"yyyy-mm-dd"
 date = datetime.datetime.now()
 date_str = date.strftime("%Y-%m-%d-%H:%M:%S")
-dirfile = os.path.abspath('/ql/data/repo/kimcrowing.github.io') # code的文件位置，我默认将其存放在根目录下
+dirfile = os.path.abspath('/ql/data/repo/kimcrowing_kimcrowing.github.io') # code的文件位置，我默认将其存放在根目录下
 repo = Repo(dirfile)
 
 g = repo.git
